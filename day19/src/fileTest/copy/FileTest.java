0a0d5b955b7da076072cd041c026268ecfde41c8
DESKTOP-KD12JUJ\Administrator <abb60455@naver.com>
2023-06-16 17:09:21 +0900

    no message

---
+++ day19/src/fileTest/copy/FileTest.java
@@ -0,0 +1,16 @@
+package fileTest.copy;
+
+import java.io.File;
+
+public class FileTest {
+	public static void main(String[] args) {
+		File f = new File("test.txt");
+		
+	}
+
+	@Override
+	public String toString() {
+		// TODO Auto-generated method stub
+		return super.toString();
+	}
+}
